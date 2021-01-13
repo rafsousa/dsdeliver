967fd4d2cc08bd4ff1c946654a78d0866218e00a
Rodrigo Alex <rodrigo.alex.sousa@gmail.com>
2021-01-12 22:31:33 -0300

    Finalizando projeto mobile

--- front-mobile/src/OrderCard/index.tsx
+++ front-mobile/src/OrderCard/index.tsx
@@ -1,37 +1,48 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { Order } from '../types';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
+dayjs.locale('pt-br');
+dayjs.extend(relativeTime);
 
-function OrderCard() {
+type Props = {
+    order: Order;
+}
 
+function dateFromNow(date: string) {
+    return dayjs(date).fromNow();
+}
+
+function formatPrice(price: number) {
+    const formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        // Quantidade de digitos depois da virgula
+        minimumFractionDigits: 2
+    });
+
+    return formatter.format(price);
+}
+
+function OrderCard({ order }: Props) {
     return (
-        <>
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Text style={styles.orderName}>
-                        Pedido 1
-                    </Text>
-                    <Text style={styles.orderPrice}>
-                        R$ 50,00
-                    </Text>
-                </View>
-                <Text style={styles.text}>
-                        Há 30 minutos
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.orderName}>Pedido {order.id}</Text>
+                <Text style={styles.orderPrice}>
+                   {order.total}
                 </Text>
-                <View style={styles.productsList}>
-                    <Text style={styles.text}>
-                            Pizza Calavresa
-                    </Text>
-                    <Text style={styles.text}>
-                            Pizza 4 queijos
-                    </Text>
-                    <Text style={styles.text}>
-                            Pizza Marguerita
-                    </Text>
-                </View>
             </View>
-        </>
-
+            <Text style={styles.text}>{dateFromNow(order.moment)}</Text>
+            <View style={styles.productsList}>
+                {order.products.map(product => (
+                    <Text key={product.id} style={styles.text}>{product.name}</Text>
+                ))}
+            </View>
+        </View>
     );
 }
 
